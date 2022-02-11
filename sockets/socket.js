c11428fd8df812ac55961bc1bda579b89eb54285
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-02-11 15:53:26 -0500

    :tada: [BEGIN] Chat server backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,41 +1,12 @@
 const { io: server } = require('../index')
-const { ADD_BAND, ADD_VOTE, GET_BANDS, DELETE_BAND } = require('../types/band.types')
 const { CONNECT, DISCONNECT } = require('../types/global.types')
-const Band = require('../models/band')
-const Bands = require('../models/bands')
-
-const bands = new Bands()
-bands.addBand(new Band('Metallica'))
-bands.addBand(new Band('Queen'))
-bands.addBand(new Band('Iron Maiden'))
-bands.addBand(new Band('Deep Purple'))
-bands.addBand(new Band('Led Zeppelin'))
 
 // Socket messages
 server.on(CONNECT, (client) => {
   console.log(`Client connected`)
 
-  // We send a message for a new client connected
-  client.emit(GET_BANDS, bands.getBands())
-
   // ON is for listening to the client
   client.on(DISCONNECT, () => {
     console.log('Client disconnected')
-  }) // Callback qhen some client disconnect from the server
-
-  // Event for add a Vote to specific band
-  client.on(ADD_VOTE, (payload) => {
-    bands.voteBand(payload.id) // Add a vote to the band
-    server.emit(GET_BANDS, bands.getBands()) // The server send all the bands again
-  })
-
-  client.on(ADD_BAND, (payload) => {
-    payload.votes ? bands.addBand(new Band(payload.name, payload.votes)) : bands.addBand(new Band(payload.name))
-    server.emit(GET_BANDS, bands.getBands())
-  })
-
-  client.on(DELETE_BAND, (payload) => {
-    bands.removeBand(payload.id) // Remove a band
-    server.emit(GET_BANDS, bands.getBands())
   })
 })
