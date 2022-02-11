2d1aee82efb3fce5e7aee4bda9cc11e344d6ff5a
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-02-11 16:44:21 -0500

    :construction: [ADD] Node and Mongo connection

--- database/config.js
+++ database/config.js
@@ -3,7 +3,11 @@ const mogoose = require('mongoose')
 // Connection
 const dbConnection = async () => {
   try {
-    console.log('Connected to database...')
+    await mogoose.connect(process.env.DB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('Database is connected')
   } catch (error) {
     console.error(error)
     throw new Error('Data base error, contact with support')
