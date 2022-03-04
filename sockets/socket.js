f7b79a09b291401df3c1ba8a79a397e9316375ba
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-03-04 07:22:56 -0500

    :art: [ADD] Listen message from Flutter

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,5 +1,5 @@
 const { io: server } = require('../index')
-const { CONNECT, DISCONNECT } = require('../types/global.types')
+const { CONNECT, DISCONNECT, CHAT_MESSAGE } = require('../types/global.types')
 const { findOutJWT } = require('../helpers/jwt')
 const { userConnected, userDisconnected } = require('../controllers/socket')
 
@@ -11,6 +11,13 @@ server.on(CONNECT, (client) => {
   if (!isValid) client.disconnect() // If the JWT is not valid, disconnect the client
 
   userConnected(uid)
+
+  // Connect user to a specific room
+  client.join(uid)
+
+  client.on(CHAT_MESSAGE, (payload) => {
+    console.log(payload)
+  })
 
   // ON is for listening to the client
   client.on(DISCONNECT, () => {
--- types/global.types.js
+++ types/global.types.js
@@ -1,7 +1,9 @@
 const CONNECT = 'connect' // Event to connect to the server
 const DISCONNECT = 'disconnect' // Event to disconnect from the server
+const CHAT_MESSAGE = 'chatMessage' // Event to send or recive a chat message
 
 module.exports = {
   CONNECT,
   DISCONNECT,
+  CHAT_MESSAGE,
 }
