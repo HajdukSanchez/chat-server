0304d58afbed918ac6ea7fba45af557c05593eb1
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2022-03-08 17:46:10 -0500

    :construction: [ADD] Message model

---
+++ models/message.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose')
+
+const MessageSchema = Schema(
+  {
+    from: {
+      type: Schema.types.ObjectId, // Hace referencia a que este campo es un ObjectId existente dentro de la base de datos
+      ref: 'User', // Se inidca la referencia de la collección con la que comparará el Type
+      required: true,
+    },
+    from: {
+      type: Schema.types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } // We add the create and edition time in the database
+)
+
+// We obtaine the output of the Model for send to the user
+MessageSchema.method('toJSON', function () {
+  const { __v, _id, ...object } = this.toObject() // We extract the object created by mongoose
+  return object
+})
+
+module.exports = model('Message', MessageSchema)
